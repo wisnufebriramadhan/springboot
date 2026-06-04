5424eb30697d2cf88946dedc8486a4c23200cb8c
wisnufebriramadhan <wisnufebriramadhan@gmail.com>
2026-06-04 14:12:38 +0700

    update json format

--- src/main/java/com/startechinnovation/userapi/entity/User.java
+++ src/main/java/com/startechinnovation/userapi/entity/User.java
@@ -25,6 +25,7 @@ public class User {
     @Column(nullable = false)
     private String role; // "ROLE_CUSTOMER" or "ROLE_ADMIN"
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "branch_id")
     private Branch branch;
